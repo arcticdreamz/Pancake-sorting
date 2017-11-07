966b96d92aaed7bb9fc05c63fb8f856326d52b22
Tom <tomcrasset@gmail.com>
2017-11-07 13:35:50 +0100

    Changed pair to tuple

--- Pancakes.cpp
+++ Pancakes.cpp
@@ -8,6 +8,7 @@
 #include "pancakes.hpp"
 #include <sstream>
 #include <utility> // for pair
+#include <tuple>
 
 using std::cout;
 using std::cin;
@@ -15,27 +16,28 @@ using std::endl;
 using std::pair;
 using std::set;
 using std::string;
+using std::get;
 
 
-typedef pair<int,int> cost;
-typedef pair<stack_type,flip_type> pancake_stack_raw;
-typedef pair<pair<stack_type,flip_type>,pair<int,int> > pancake_stack;
+//typedef pair<int,int> cost;
+//typedef pair<stack_type,flip_type> pancake_stack_raw;
+//typedef pair<pair<stack_type,flip_type>,pair<int,int> > pancake_stack;
+
+typedef std::tuple<int,int,stack_type,flip_type> pancake_stack; // real cost, cout est, 
 
 //Compare function for the set
-bool comp(const pancake_stack stackA, const pancake_stack stackB){
+ bool comp(const pancake_stack stackA, const pancake_stack stackB){
 
-    int totalCostA = stackA.second.first + stackA.second.second;
-    int totalCostB = stackB.second.first + stackB.second.second;
-    if(totalCostA == totalCostB){
-        return stackA < stackB;
-    }
-    return totalCostA < totalCostB ;
-}
+     int totalCostA = get<0>(stackA)+ get<1>(stackA);
+     int totalCostB = get<0>(stackB)+ get<1>(stackB);
+     if(totalCostA == totalCostB){
+         return stackA < stackB;
+     }
+     return totalCostA < totalCostB ;
+ }	
 
 
 // Get the estimated cost of a stack; the size of the largest pancake not yet at its place
-
-
 stack_type::iterator getMax(stack_type& stackA) {
     stack_type::iterator maximum;
     stack_type::size_type i = 0;
@@ -51,14 +53,15 @@ stack_type::iterator getMax(stack_type& stackA) {
 }
 
 int getEstCost(pancake_stack& stackA)  {
-    return *(getMax(stackA.first.first));
+    return *(getMax(get<2>(stackA)));
 }
 
 
 
 string stackToString(pancake_stack& stackA) {
+	stack_type vector = get<2>(stackA);
 	std::stringstream result;
-	std::copy(stackA.first.first.begin(), stackA.first.first.end(), std::ostream_iterator<int>(result, ""));
+	std::copy(vector.begin(), vector.end(), std::ostream_iterator<int>(result, ""));
 	std::string pancakestring = result.str();
 	return pancakestring;
 }
@@ -80,9 +83,7 @@ void astar_pancake_sort(const stack_type& pancakes, flip_type& flips) {
     std::copy(pancakes.begin(),pancakes.end(),back_inserter(pancakes_copy));
 
     //Initialise the stacks
-    cost zeroCost = std::make_pair(0,0); // Real cost , EstCost
-    pancake_stack_raw initial_raw_stack = make_pair(pancakes_copy,flips);
-    pancake_stack initial_stack = make_pair(initial_raw_stack,zeroCost);
+    pancake_stack initial_stack = std::make_tuple(0,0,pancakes_copy,flips);
 
     //Create a sorted stack to compare
     stack_type sorted_stack = pancakes_copy;
@@ -93,51 +94,51 @@ void astar_pancake_sort(const stack_type& pancakes, flip_type& flips) {
 
     p_queue.insert(initial_stack); //First call
 
-    set<string> stringStackVector;
+    set<string> stringStackSet;
 
     set<string>::iterator foundString;
 
-    while(!std::equal(parent_stack.first.first.begin(),parent_stack.first.first.end(),sorted_stack.begin())) {
+    while(!std::equal(get<2>(parent_stack).begin(),get<2>(parent_stack).end(),sorted_stack.begin())) {
 
         if(!p_queue.empty()){ //don't want to dereference end pointer
         	parent_stack = *(p_queue.begin()); //Extract from the priority queue
             p_queue.erase(p_queue.begin()); //Delete the extracted element from the priority queue
         }
 
-        if(stringStackVector.empty()) {
-        	stringStackVector.insert(stackToString(initial_stack));
+        if(stringStackSet.empty()) {
+        	stringStackSet.insert(stackToString(initial_stack));
         }else{
-        	//Looking in the stringStackVector if the stack hasn't already been encountered
-	        foundString = std::find(stringStackVector.begin(), stringStackVector.end(),stackToString(parent_stack));
-	      	if(foundString != stringStackVector.end()){
+        	//Looking in the stringStackSet if the stack hasn't already been encountered
+	        foundString = std::find(stringStackSet.begin(), stringStackSet.end(),stackToString(parent_stack));
+	      	if(foundString != stringStackSet.end()){
 	//            cout << *foundString << " has already been generated" << endl;
 	            continue;
 			}
         }
-     	//cout << "Pushing parent into the stringStackVector" << endl;
-        stringStackVector.insert(stackToString(parent_stack));
+     	//cout << "Pushing parent into the stringStackSet" << endl;
+        stringStackSet.insert(stackToString(parent_stack));
 
         // generate children from parent stack
-        for(stack_type::size_type i = 1; i < parent_stack.first.first.size(); i++) { // generate children from parent stack
+        for(stack_type::size_type i = 1; i < get<2>(parent_stack).size(); i++) { // generate children from parent stack
 
             pancake_stack child_stack = parent_stack;
 
-            child_stack.first.second.push_back(i); //Pushing the flip
-            std::reverse(child_stack.first.first.begin(),child_stack.first.first.begin()+ 1 + i); // flip at index i (i+1 because reverse() second argument is non-inclusive)
+            get<3>(child_stack).push_back(i); //Pushing the flip
+            std::reverse(get<2>(child_stack).begin(),get<2>(child_stack).begin()+ 1 + i); // flip at index i (i+1 because reverse() second argument is non-inclusive)
 
-            //Looking in the stringStackVector if the stack hasn't already been encountered
-            foundString = std::find(stringStackVector.begin(), stringStackVector.end(),stackToString(child_stack));
+            //Looking in the stringStackSet if the stack hasn't already been encountered
+            foundString = std::find(stringStackSet.begin(), stringStackSet.end(),stackToString(child_stack));
 
             //If the stack has indeed already been encountered before, we just loop again
-            if(foundString != stringStackVector.end()){
+            if(foundString != stringStackSet.end()){
                 continue;
             }
 
             //Computing the cost
             int realCost = i+1; // i+1 pancakes were flipped
             int estCost = getEstCost(child_stack); //Compute the estimated end cost
-            child_stack.second.first += realCost; // Add the child's realCost to the parent's realCost
-            child_stack.second.second = estCost;
+            get<0>(child_stack) += realCost; // Add the child's realCost to the parent's realCost
+            get<1>(child_stack) = estCost;
 
             //Insert child into the set
             p_queue.insert(child_stack);
@@ -145,9 +146,9 @@ void astar_pancake_sort(const stack_type& pancakes, flip_type& flips) {
     }
 
     cout << "The size of the priority queue is : " << p_queue.size() << endl;
-    cout << "The best cost was: " << parent_stack.second.first + parent_stack.second.second <<endl;
+    cout << "The best cost was: " << get<0>(parent_stack) + get<1>(parent_stack) <<endl;
     cout << " The flips vector contains : ";
-    for(flip_type::iterator it = parent_stack.first.second.begin(); it != parent_stack.first.second.end() ; it++ ) {
+    for(flip_type::iterator it = get<3>(parent_stack).begin(); it != get<3>(parent_stack).end() ; it++ ) {
         cout << ' '<<*it ;
     }
     cout << endl;
@@ -251,9 +252,9 @@ void simple_pancake_sort(const stack_type& pancakes, flip_type& flips){
 //
 
 
-//    cout << "The stringStackVector contains : " <<  endl;
-//   sort(stringStackVector.begin(),stringStackVector.end());
-//    for(std::vector<string>::iterator it = stringStackVector.begin(); it != stringStackVector.end() ; it++ ) {
+//    cout << "The stringStackSet contains : " <<  endl;
+//   sort(stringStackSet.begin(),stringStackSet.end());
+//    for(std::vector<string>::iterator it = stringStackSet.begin(); it != stringStackSet.end() ; it++ ) {
 //        cout << ' '<<*it << endl; ;
 //    }
 //    cout << endl;
--- main.cpp
+++ main.cpp
@@ -18,9 +18,15 @@ int main()
     v.push_back(1);
     v.push_back(4);
     v.push_back(2);
+	v.push_back(9);	
+	v.push_back(7);
+	v.push_back(10)
+	v.push_back(6);
+	v.push_back(8);
+
     v.push_back(3);
 
-    v.push_back(0);
+
 
 
 std::cout << "Initial Stack : " << std::endl;
